79e262bf327aecc6580d89615b443896e6832bfb
App Academy Student <student@appacademy.io>
2013-10-29 15:39:40 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JournalApp::Application.config.secret_token = '2420b3b6dfb02dd2b258fbdf5cc263b8c638ed5e69a7620d540bfad069147261cf1e301e2d7e6c406d94ddf1cd5b5135fcb252df3b8f1547ecda8245f47632da'
