8e3e672d11189a17423d537edaf86ca50e90eb7f
App Academy Student <student@appacademy.io>
2013-10-29 16:11:14 -0700

    Adds Post model (i.e. server/client)

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,30 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+
+    respond_to do |format|
+      format.html {render :index}
+      format.json {render json: @posts}
+    end
+  end
+
+  def create
+    @post = Post.new(params[:post])
+
+    @post.save!
+
+    respond_to do |format|
+      # format.html {render}
+      format.json {render json: @post}
+    end
+  end
+
+  def destroy
+    # @post = Post.all
+
+    # respond_to do |format|
+#       format.html {render}
+#       format.json {render json: @post}
+#     end
+  end
+end
